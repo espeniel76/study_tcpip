e3769665514a5006af71f86d4adc64164b4c3238
espeniel76 <espeniel69@gmail.com>
2021-05-14 22:54:09 +0900

    .

--- chapter03/inet_addr.c
+++ chapter03/inet_addr.c
@@ -3,17 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-	char *addr1="127.212.124.78";
-	char *addr2="127.212.124.256";
+	char *addr1 = "127.212.124.78";
+	char *addr2 = "127.212.124.256";
 
-	unsigned long conv_addr=inet_addr(addr1);
-	if(conv_addr==INADDR_NONE)
+	unsigned long conv_addr = inet_addr(addr1);
+	if (conv_addr == INADDR_NONE)
 		printf("Error occured! \n");
 	else
 		printf("Network ordered integer addr: %#lx \n", conv_addr);
-	
-	conv_addr=inet_addr(addr2);
-	if(conv_addr==INADDR_NONE)
+
+	conv_addr = inet_addr(addr2);
+	if (conv_addr == INADDR_NONE)
 		printf("Error occureded \n");
 	else
 		printf("Network ordered integer addr: %#lx \n\n", conv_addr);
--- chapter03/inet_aton.c
+++ chapter03/inet_aton.c
@@ -5,10 +5,10 @@ void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-	char *addr="127.232.124.79";
+	char *addr = "127.232.124.79";
 	struct sockaddr_in addr_inet;
-	
-	if(!inet_aton(addr, &addr_inet.sin_addr))
+
+	if (!inet_aton(addr, &addr_inet.sin_addr))
 		error_handling("Conversion error");
 	else
 		printf("Network ordered integer addr: %#x \n", addr_inet.sin_addr.s_addr);
