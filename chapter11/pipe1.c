48d47950b7bf72ec3b1d8b6bdad7e283bd303900
espeniel76 <espeniel69@gmail.com>
2021-05-27 09:01:34 +0900

    .

--- chapter11/pipe1.c
+++ chapter11/pipe1.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 #define BUF_SIZE 30
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int fds[2];
 	char str[] = "Who are you?";
 	char buf[BUF_SIZE];
@@ -10,16 +11,22 @@ int main(int argc, char *argv[]) {
 
 	// 이놈은 뭐지?
 	// fds 배열에 파이프 생성
-	// 두개의 file 디스크립터가 생성됨
+	// 두개의 pipe file 디스크립터가 생성됨
 	pipe(fds);
+	// fds[0] 파이프의 출구다.
+	// fds[1] 파이프의 입구다.
 
 	// 자식 프로세스 생성
 	pid = fork();
 
-	if (pid == 0) {
+	if (pid == 0)
+	{
+		// 자식 프로세스가 fds 파이프를 통해 문자를 집어넣고 있다.
 		write(fds[1], str, sizeof(str));
-	} else {
-		int i;
+	}
+	else
+	{
+		// 부모 프로세스가 fds 파이프를 통해 문자열을 받고있다.
 		read(fds[0], buf, BUF_SIZE);
 		puts(buf);
 	}
--- chapter11/pipe2.c
+++ chapter11/pipe2.c
@@ -19,15 +19,23 @@ int main(int argc, char *argv)
 	// child process
 	if (pid == 0)
 	{
+		// fds 입구를 통해 문자열을 넣는다.
 		write(fds[1], str_child, sizeof(str_child));
-		sleep(2);
+		
+		// 2초 딜레이 준다.
+		// sleep(2);
+
+		// fds 출구를 통해 문자열을 받는다.
 		read(fds[0], buf, BUF_SIZE);
 		printf("Child proc output: %s \n", buf);
 	}
 	else // parent process
 	{
+		// fds 출구를 통해 문자를 받는다.
 		read(fds[0], buf, BUF_SIZE);
 		printf("Parent proc output: %s \n", buf);
+		
+		// fds 입구를 통해 문자를 넣는다.
 		write(fds[1], str_parent, sizeof(str_parent));
 		sleep(3);
 	}
